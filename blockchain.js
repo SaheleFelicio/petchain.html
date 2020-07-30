ba9a4a5976349bec87ea35ca23bc8c0c7b21a23f
Sahelê Felicio <67347282+SaheleFelicio@users.noreply.github.com>
2020-07-30 15:31:19 -0300

    c correções ids

--- blockchain.js
+++ blockchain.js
@@ -1,14 +1,39 @@
-function registroDoAnimal() {
+function registroDoAnimal(_registro) {
     event.preventDefault();
-    if ($("#_tutor","#_criador").val().length != 42) {
-      $("#_tutor","#_criador").focus();
+    
+    if ($("#_nome").val().length < 2) {
+      alert("Nome do proprietário inválido");
+      $("#_nomeProprietario").focus();
+      return;
+    }
+
+    if ($("#_especie").val().length < 2) {
+      alert("Espécie inválida");
+      $("#_especie").focus();
+      return;
+    }
+
+    if ($("#_sexo").val().length < 2) {
+      alert("Sexo inválido");
+      $("#_sexo").focus();
+      return;
+    }
+
+    if (!$("#_criador").val().startsWith("0x")) {
       alert("Endereço inválido");
+      $("#_criador").focus();
       return;
     }
   
-    if (!$("#_tutor","#_criador").val().startsWith("0x")) {
+    if (!$("#_tutor").val().startsWith("0x")) {
       alert("Endereço inválido");
-      $("#_tutor","#_criador").focus();
+      $("#_tutor").focus();
+      return;
+    }
+
+    if ($("#_LocalDeNascimento").val().length < 2) {
+      alert("Local de nascimento inválido");
+      $("#_LocalDeNascimento").focus();
       return;
     }
     
@@ -16,20 +41,21 @@ function registroDoAnimal() {
       alert("Você não está conectado ao Ethereum. Verifique seu Metamask");
       return;
     }
-  
+    
+      $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
+      $("#statusTransacoes").show();
     contratoComSignatario
-      .registroDoAnimal($("#_nome").val(), $("#_especie").val(), $("#_sexo").val(), $("#_criador").val(), $("#_tutor").val(), $("#_LocalDeNascimento").val(), $("#_dataDeNascimento").val(), $("#_registro").val() * 1)
+      .registroDoAnimal($(_nome).val(), $(_especie).val(), $(_sexo).val(), $(_criador).val(), $(_tutor).val(), $(_LocalDeNascimento).val(), $(_certificado).val(), ($(_dataDeNascimento).val() * 1))
       .then((transacao) => {
-        $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
-        $("#statusTransacoes").toggle();
         transacao
           .wait()
           .then((resultado) => {
             console.log("registro - o resultado foi ", resultado);
             if (resultado.status === 1) {
-              $("#descricaoStatusTransacoes").html("Transação executada.");
+              $("#descricaoStatusTransacoes").html("Pet registrado eternamente");
+              $("#btnregistroDoAnimal").prop(true);              
             } else {
-              $("#descricaoStatusTransacoes").html("Houve um erro na execução da transação no Ethereum.");
+              $("#descricaoStatusTransacoes").html("Houve um erro no registro: " + resultado);
             }
           })
           .catch((err) => {
