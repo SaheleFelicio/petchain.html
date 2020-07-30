89b1f49819dd9177e1137b127d2fd6dd8c8a7af1
Sahelê Felicio <67347282+SaheleFelicio@users.noreply.github.com>
2020-07-30 18:28:29 -0300

    sem certificado

--- dados-contratos-inteligentes.js
+++ dados-contratos-inteligentes.js
@@ -1,118 +1,108 @@
-var enderecoContrato = "0x789DBcB6923BC51A507B9C9D04Be802D75334644";
+var enderecoContrato = "0x47e8da7E34c049A5aFE13d276a089dA90a539A88";
 var abiContrato = [
+	{
+		"inputs": [
 			{
-			"inputs": [
-				{
-					"internalType": "string",
-					"name": "_nome",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "_especie",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "_sexo",
-					"type": "string"
-				},
-				{
-					"internalType": "address",
-					"name": "_criador",
-					"type": "address"
-				},
-				{
-					"internalType": "address",
-					"name": "_tutor",
-					"type": "address"
-				},
-				{
-					"internalType": "string",
-					"name": "_LocalDeNascimento",
-					"type": "string"
-				},
-				{
-					"internalType": "uint256",
-					"name": "_dataDeNascimento",
-					"type": "uint256"
-				},
-				{
-					"internalType": "string",
-					"name": "_certificado",
-					"type": "string"
-				}
-			],
-			"name": "registroDoAnimal",
-			"outputs": [],
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "uint256",
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"name": "animais",
-			"outputs": [
-				{
-					"internalType": "string",
-					"name": "nome",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "especie",
-					"type": "string"
-				},
-				{
-					"internalType": "string",
-					"name": "sexo",
-					"type": "string"
-				},
-				{
-					"internalType": "address",
-					"name": "criador",
-					"type": "address"
-				},
-				{
-					"internalType": "address",
-					"name": "tutor",
-					"type": "address"
-				},
-				{
-					"internalType": "string",
-					"name": "LocalDeNascimento",
-					"type": "string"
-				},
-				{
-					"internalType": "uint256",
-					"name": "dataDeNascimento",
-					"type": "uint256"
-				},
-				{
-					"internalType": "string",
-					"name": "certificado",
-					"type": "string"
-				}
-			],
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"inputs": [],
-			"name": "totalDeAnimais",
-			"outputs": [
-				{
-					"internalType": "uint256",
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"stateMutability": "view",
-			"type": "function"
-		}
-	];
+				"internalType": "string",
+				"name": "_nome",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_especie",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_sexo",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_criador",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_tutor",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_LocalDeNascimento",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_dataDeNascimento",
+				"type": "uint256"
+			}
+		],
+		"name": "registroDoAnimal",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "animais",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "nome",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "especie",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "sexo",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "criador",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "tutor",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "LocalDeNascimento",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "dataDeNascimento",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalDeAnimais",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
